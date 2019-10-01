5fb4b73bc074c9a4359e508eb184a1b16fc7c39f
Devany <devanydc>
2019-10-01 14:00:31 +0700

    Fragment for Handstand is Done

--- app/src/main/java/polinema/ac/id/starterchapter05/fragments/HandstandFragment.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/fragments/HandstandFragment.java
@@ -2,33 +2,31 @@ package polinema.ac.id.starterchapter05.fragments;
 
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import polinema.ac.id.starterchapter05.R;
 
+import polinema.ac.id.starterchapter05.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
+public class HandstandFragment extends Fragment {
 
-public class DipsFragment extends Fragment {
-    public DipsFragment() {
 
+    public HandstandFragment() {
         // Required empty public constructor
-
     }
+
 
     @Override
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_dips, container, false);
-
+        return inflater.inflate(R.layout.fragment_handstand, container, false);
     }
+
 }
